aec70f836ab5ad43777024250ea6fc4f2c686c57
Vaida Marius <103062649+BukiCl@users.noreply.github.com>
2022-11-08 10:26:18 +0200

    have to fix some bugs with the images

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Hotels from "../components/body/Hotels";
+import Navbar from "../components/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hotels />
+    </>
+  );
+};
+
+export default Home;
